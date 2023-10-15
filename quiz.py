55c09a9451a85a4c369c03986c06d9a0658680fc
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-15 18:55:48 -0500

    List of Questions

---
+++ quiz.py
@@ -0,0 +1,5 @@
+QUESTIONS = [
+    ("What is the color for: Stop, yield, or do what is shown on the sign.", "red"),
+    ("What is the color for: Direction. Indicates where a place is, or how far a place is from where you are.", "green"),
+    ("What is the color for: Regulatory. Provides information regarding enforceable laws and ordinances.", "white")
+]
