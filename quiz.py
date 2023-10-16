86995cee25af6acb2c7f5deca6bb3699995c95a5
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-15 19:02:33 -0500

    a for loop that checks the input answer correct or not

--- quiz.py
+++ quiz.py
@@ -1,5 +1,12 @@
 QUESTIONS = [
-    ("What is the color for: Stop, yield, or do what is shown on the sign.", "red"),
-    ("What is the color for: Direction. Indicates where a place is, or how far a place is from where you are.", "green"),
-    ("What is the color for: Regulatory. Provides information regarding enforceable laws and ordinances.", "white")
+    ("What is the color for: Stop, yield, or do what is shown on the sign", "red"),
+    ("What is the color for: Direction. Indicates where a place is, or how far a place is from where you are", "green"),
+    ("What is the color for: Regulatory. Provides information regarding enforceable laws and ordinances", "white")
 ]
+
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question} ? ")
+    if answer.lower() == correct_answer:
+        print("Ding ding! Correct!")
+    else:
+        print(f"The answer if {correct_answer}, not {answer}.")
