ced7947962b7085df8953da2153ca2f964de63df
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-15 19:20:05 -0500

    Change system into a dictionary of questions with alternatives of answers

--- quiz.py
+++ quiz.py
@@ -1,10 +1,14 @@
-QUESTIONS = [
-    ("What is the color for: Stop, yield, or do what is shown on the sign", "red"),
-    ("What is the color for: Direction. Indicates where a place is, or how far a place is from where you are", "green"),
-    ("What is the color for: Regulatory. Provides information regarding enforceable laws and ordinances", "white")
-]
+QUESTIONS = {
+    "What is the color for: Stop, yield, or do what is shown on the sign" : ["red", "white", "green", "yellow"],
+    "What is the color for: Direction. Indicates where a place is, or how far a place is from where you are" : ["green", "white", "red", "yellow"],
+    "What is the color for: Regulatory. Provides information regarding enforceable laws and ordinances" : ["white", "red", "green", "yellow"]
+}
 
-for question, correct_answer in QUESTIONS:
+for question, alternatives in QUESTIONS.items():
+    correct_answer = alternatives[0]
+    for alternative in sorted(alternatives):
+        print(f" - {alternative}")
+
     answer = input(f"{question} ? ")
     if answer.lower() == correct_answer:
         print("Ding ding! Correct!")
